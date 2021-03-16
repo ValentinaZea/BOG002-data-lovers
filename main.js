ce3da765af854c6d60c765af321ef48a9adad45f
Valentina Zea <mavalezea@gmail.com>
2021-03-16 11:32:25 -0500

    Pintar tarjetas

--- main.js
+++ main.js
@@ -7,7 +7,15 @@ for (let i = 0; i < data.results.length; i++) {
   //   console.log(data.results[i]["id"]);
   //   console.log(data.results[i]["name"]);
   // nombre.innerHTML += `nombre:${data.results[i]["image"]}<br>`;
-  nombre.innerHTML += `<img loading="lazy" src="${data.results[i]["image"]}"/> <br> ${data.results[i]["name"]} <br>`;
+    var newDiv = document.createElement('div');
+    // document.getElementsByClassName('character-card');
+    var img = document.createElement('img'); 
+    img.src = data.results[i]["image"];
+    var name = document.createTextNode(data.results[i]["name"]); 
+    newDiv.appendChild(img); 
+    newDiv.appendChild(name);
+    document.getElementById("root").appendChild(newDiv);
+
 }
 
 console.log(data);
