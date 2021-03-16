3d030c7bfce62659fbcc1511639b4fae77f79b69
Tatiroa994 <tatico994@gmail.com>
2021-03-16 17:52:54 -0500

    card

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 // import { example } from "./data.js";
 import data from "./data/rickandmorty/rickandmorty.js";
 
-let root = document.getElementById("root");
+const root = document.getElementById("root");
+const btnCharacter = document.getElementById("btn-character");
 
-for (let i = 0; i < data.results.length; i++) {
-  let name = data.results[i]["name"];
-  let image = data.results[i]["image"];
-  let newDiv = document.createElement("div");
-  let myImage = new Image(200, 200);
-  myImage.src = image;
-  newDiv.insertAdjacentHTML("beforeend", name); 
-  newDiv.insertAdjacentElement("afterbegin", myImage);
-  root.appendChild(newDiv);
+btnCharacter.addEventListener("click", () => {
+  for (let i = 0; i < data.results.length; i++) {
+    let name = data.results[i]["name"];
+    let image = data.results[i]["image"];
+    let newDiv = document.createElement("div");
+    let myImage = new Image(200, 200);
+    myImage.src = image;
+    newDiv.insertAdjacentHTML("beforeend", name);
+    newDiv.insertAdjacentElement("afterbegin", myImage);
+    root.appendChild(newDiv);
+    newDiv.classList.add("card");
   }
+});
