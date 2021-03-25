5cb4a6b474c6c3c279a397181914fff1b622df68
Tatiroa994 <tatico994@gmail.com>
2021-03-25 13:59:00 -0500

    card character

--- src/data.js
+++ src/data.js
@@ -1,13 +1,5 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return "example";
-};
-
-export const anotherExample = () => {
-  return "OMG";
-};
-
 export const searchByName = (dataOriginal, input) => {
   let characterFound = [];
   dataOriginal.forEach((element) => {
@@ -18,46 +10,25 @@ export const searchByName = (dataOriginal, input) => {
 };
 
 export const getCharacter = (dataOriginal, idCard) => {
-  for (let i = 0; i < dataOriginal.length; i++) {
+ let newList = {};
+    for (let i = 0; i < dataOriginal.length; i++) {
     if (dataOriginal[i]["id"] === idCard) {
-      delete dataOriginal[i].id;
-      delete dataOriginal[i].created;
-      delete dataOriginal[i].url;
-      delete dataOriginal[i].image;
-      delete dataOriginal[i].type;
-      let newLocation = dataOriginal[i]["location"].name;
-      Object.defineProperty(dataOriginal[i], "location", {
-        value: newLocation,
-      });
-      let newOrigin = dataOriginal[i]["origin"].name;
-      Object.defineProperty(dataOriginal[i], "origin", { value: newOrigin });
-      let lengthEpisode = dataOriginal[i]["episode"].length;
-      Object.defineProperty(dataOriginal[i], "episode", {
-        value: lengthEpisode,
-      });
-      return dataOriginal[i];
-
-      //   let keys= Object.keys(dataOriginal[i])
-      //  console.log(keys)
-      //   switch (keys) {
-      //     case 0:
-      //       delete dataOriginal[i].id;
-      //       break;
-      //     case 11:
-      //       delete dataOriginal[i].created;
-      //       break;
-      //     case 10:
-      //       delete dataOriginal[i].url;
-      //       break;
-      //     case 8:
-      //       delete dataOriginal[i].image;
-      //       console("ingreso  a  image");
-      //       break;
-      //     default:
-      //     console.log(dataOriginal[i]);
-      //   }
-
-      //console.log(Object.keys(dataOriginal[i]));
+      newList = {
+      "info": { 
+        Nombre : dataOriginal[i].name,
+        Estado: dataOriginal[i].status,
+        Especie: dataOriginal[i].species,
+        Genero: dataOriginal[i].gender, 
+        Origen: dataOriginal[i]["origin"].name,
+        Locación: dataOriginal[i]["location"].name, 
+        Episodios: dataOriginal[i]["episode"].length
+      },
+      "image":{
+         url: dataOriginal[i].image
+        },
+      }
     }
   }
+  return newList;
 };
+
