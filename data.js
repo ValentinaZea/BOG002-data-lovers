a104a03e1f9fa9b453789fafe8cda51f50c11a60
Valentina Zea <mavalezea@gmail.com>
2021-04-05 13:17:59 -0500

    Updates

--- data.js
+++ data.js
@@ -1,31 +1,33 @@
 import data from "./data/rickandmorty/rickandmorty.js";
-export const searchByName = (dataOriginal, input) => {
+
+export const searchByName = (data, input) => {
   //barra de busqueda
   let characterFound = [];
-  dataOriginal.forEach((element) => {
+  data.forEach((element) => {
     if (element.name.toLowerCase().includes(input))
       characterFound.push(element);
   });
+  console.log(characterFound);
   return characterFound;
 };
 
-export const getCharacter = (dataOriginal, idCard) => {
+export const getCharacter = (idCard) => {
   // datos personaje por tarjeta
   let newList = {};
-  for (let i = 0; i < dataOriginal.length; i++) {
-    if (dataOriginal[i]["id"] === idCard) {
+  for (let i = 0; i < data.results.length; i++) {
+    if (data.results[i]["id"] === idCard) {
       newList = {
-        info: {
-          Nombre: dataOriginal[i].name,
-          Estado: dataOriginal[i].status,
-          Especie: dataOriginal[i].species,
-          Genero: dataOriginal[i].gender,
-          Origen: dataOriginal[i]["origin"].name,
-          Locación: dataOriginal[i]["location"].name,
-          "Episodios de aparición": dataOriginal[i]["episode"].length,
-        },
         image: {
-          url: dataOriginal[i].image,
+          url: data.results[i].image,
+        },
+        info: {
+          Nombre: data.results[i].name,
+          Estado: data.results[i].status,
+          Especie: data.results[i].species,
+          Genero: data.results[i].gender,
+          Origen: data.results[i]["origin"].name,
+          Locación: data.results[i]["location"].name,
+          "Episodios de aparición": data.results[i]["episode"].length,
         },
       };
     }
@@ -36,12 +38,15 @@ export const getCharacter = (dataOriginal, idCard) => {
 export const sortData = (data, sortOrder) => {
   // ordenar a la az
   data.sort((a, b) => {
+    // Si a es mayor que b 1 hace que b vaya antes que a
     if (a.name > b.name) {
       return 1;
     }
+    // Si a es menor que b -1 hace que a vaya antes que b
     if (a.name < b.name) {
       return -1;
     }
+    // Si son iguales retorna cero, no hay cambio de posiciones
     return 0;
   });
   if (sortOrder !== "asc") {
@@ -54,7 +59,7 @@ export function filterDataCategories(filteredData, filter) {
   let count = 0;
   const keys = Object.keys(filter);
   if (keys.length === 0) {
-    return data.results;
+    return filteredData;
   }
   keys.forEach((key) => {
     const filterByCategory = filteredData.filter(
@@ -66,5 +71,6 @@ export function filterDataCategories(filteredData, filter) {
   if (count === keys.length) {
     return filteredData;
   }
-  filterDataCategories(filteredData, filter);
+  // filterDataCategories(filteredData, filter);
+  // console.log(filterDataCategories(filteredData, filter));
 }
